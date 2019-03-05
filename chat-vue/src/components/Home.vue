a861ea162defb2649fe0c4935975df63e6d90c15
Art <artela1992@gmail.com>
2019-03-05 18:04:17 +0300

    Доаботана авторизация(урок 5)

---
+++ chat-vue/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Chat на vue js 1</h1>
+        <button @click="goLogin()">Login</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        methods:{
+            goLogin(){
+                return this.$router.push({name:'login'})
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ chat-vue/src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <input v-model="login" placeholder="Login" type="text">
+        <input v-model="password" placeholder="Password" type="password">
+        <button @click="setLogin">Войти</button>
+        <hr>
+        {{login}}:{{password}}
+    </div>
+</template>
+
+<script>
+    import $ from 'jquery'
+    export default {
+        name: "Login",
+        data(){
+            return{
+                login:'',
+                password:''
+            }
+        },
+        methods:{
+            setLogin(){
+                $.ajax({
+                    url:'http://127.0.0.1:8011/auth/token/create/',
+                    type:'POST',
+                    data:{
+                        username:this.login,
+                        password: this.password
+                    },
+                    success:(response) =>{
+                        alert('Вход выполнен успешно');
+                        sessionStorage.setItem('auth_token',response.data.attributes.auth_token);
+                    },
+                    error:(response) =>{
+                        if (response.status == 400) {
+                            console.log(response)
+                            alert('Неверный логин либо пароль')
+                        }
+                    }
+                })
+            }
+        }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
